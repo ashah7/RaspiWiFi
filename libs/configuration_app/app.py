9cdd5badcb2475df1b4595ec61d638b39f9207a9
ashah7 <ashah7@users.noreply.github.com>
2020-04-17 12:26:30 +0100

    Update app.py

--- libs/configuration_app/app.py
+++ libs/configuration_app/app.py
@@ -1,8 +1,11 @@
+#set for use with wlan1
+
 from flask import Flask, render_template, request
 import subprocess
 import os
 import time
 from threading import Thread
+import fileinput
 
 app = Flask(__name__)
 app.debug = True
@@ -11,13 +14,19 @@ app.debug = True
 @app.route('/')
 def index():
     wifi_ap_array = scan_wifi_networks()
+    config_hash = config_file_hash()
 
-    return render_template('app.html', wifi_ap_array = wifi_ap_array)
+    return render_template('app.html', wifi_ap_array = wifi_ap_array, config_hash = config_hash)
 
 
 @app.route('/manual_ssid_entry')
 def manual_ssid_entry():
     return render_template('manual_ssid_entry.html')
+
+@app.route('/wpa_settings')
+def wpa_settings():
+    config_hash = config_file_hash()
+    return render_template('wpa_settings.html', wpa_enabled = config_hash['wpa_enabled'], wpa_key = config_hash['wpa_key'])
 
 
 @app.route('/save_credentials', methods = ['GET', 'POST'])
@@ -26,7 +35,7 @@ def save_credentials():
     wifi_key = request.form['wifi_key']
 
     create_wpa_supplicant(ssid, wifi_key)
-    
+
     # Call set_ap_client_mode() in a thread otherwise the reboot will prevent
     # the response from getting to the browser
     def sleep_and_start_ap():
@@ -36,6 +45,28 @@ def save_credentials():
     t.start()
 
     return render_template('save_credentials.html', ssid = ssid)
+
+
+@app.route('/save_wpa_credentials', methods = ['GET', 'POST'])
+def save_wpa_credentials():
+    config_hash = config_file_hash()
+    wpa_enabled = request.form.get('wpa_enabled')
+    wpa_key = request.form['wpa_key']
+
+    if str(wpa_enabled) == '1':
+        update_wpa(1, wpa_key)
+    else:
+        update_wpa(0, wpa_key)
+
+    def sleep_and_reboot_for_wpa():
+        time.sleep(2)
+        os.system('reboot')
+
+    t = Thread(target=sleep_and_reboot_for_wpa)
+    t.start()
+
+    config_hash = config_file_hash()
+    return render_template('save_wpa_credentials.html', wpa_enabled = config_hash['wpa_enabled'], wpa_key = config_hash['wpa_key'])
 
 
 
@@ -60,29 +91,47 @@ def create_wpa_supplicant(ssid, wifi_key):
 
     temp_conf_file.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
     temp_conf_file.write('update_config=1\n')
+    temp_conf_file.write('country=GB\n')   #ashah appended here
     temp_conf_file.write('\n')
     temp_conf_file.write('network={\n')
-    temp_conf_file.write('	ssid="' + ssid + '"\n')
+    temp_conf_file.write('      ssid="' + ssid + '"\n')
 
     if wifi_key == '':
-        temp_conf_file.write('	key_mgmt=NONE\n')
+        temp_conf_file.write('  key_mgmt=NONE\n')
     else:
-        temp_conf_file.write('	psk="' + wifi_key + '"\n')
+        temp_conf_file.write('  psk="' + wifi_key + '"\n')
 
-    temp_conf_file.write('	}')
+    temp_conf_file.write('      }')
 
     temp_conf_file.close
 
-    os.system('mv wpa_supplicant.conf.tmp /etc/wpa_supplicant/wpa_supplicant.conf')
+    os.system('mv wpa_supplicant.conf.tmp /etc/wpa_supplicant/wpa_supplicant-wlan1.conf')   #appended here ashah
 
 def set_ap_client_mode():
-    os.system('rm /etc/cron.raspiwifi/aphost_bootstrapper')
-    os.system('cp /usr/lib/raspiwifi/reset_device/static_files/apclient_bootstrapper /etc/cron.raspiwifi/')
-    os.system('chmod +x /etc/cron.raspiwifi/apclient_bootstrapper')
-    os.system('mv /etc/dnsmasq.conf.original /etc/dnsmasq.conf')
-    os.system('mv /etc/dhcpcd.conf.original /etc/dhcpcd.conf')
-    os.system('cp /usr/lib/raspiwifi/reset_device/static_files/isc-dhcp-server.apclient /etc/default/isc-dhcp-server')
+#    os.system('rm -f /etc/raspiwifi/host_mode')   #removed ashah
+#    os.system('rm /etc/cron.raspiwifi/aphost_bootstrapper') #removed ashah
+#    os.system('cp /usr/lib/raspiwifi/reset_device/static_files/apclient_bootstrapper /etc/cron.raspiwifi/') #removed ashah
+#    os.system('chmod +x /etc/cron.raspiwifi/apclient_bootstrapper') #removed ashah
+#    os.system('mv /etc/dnsmasq.conf.original /etc/dnsmasq.conf') #removed ashah
+#    os.system('mv /etc/dhcpcd.conf.original /etc/dhcpcd.conf') #removed ashah
     os.system('reboot')
+
+def update_wpa(wpa_enabled, wpa_key):
+    with fileinput.FileInput('/etc/raspiwifi/raspiwifi.conf', inplace=True) as raspiwifi_conf:
+        for line in raspiwifi_conf:
+            if 'wpa_enabled=' in line:
+                line_array = line.split('=')
+                line_array[1] = wpa_enabled
+                print(line_array[0] + '=' + str(line_array[1]))
+
+            if 'wpa_key=' in line:
+                line_array = line.split('=')
+                line_array[1] = wpa_key
+                print(line_array[0] + '=' + line_array[1])
+
+            if 'wpa_enabled=' not in line and 'wpa_key=' not in line:
+                print(line, end='')
+
 
 def config_file_hash():
     config_file = open('/etc/raspiwifi/raspiwifi.conf')
